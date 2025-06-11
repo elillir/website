c3c0cdf0dadd099f6858c57a02119ad6c098a56a
elillir <168267060+elillir@users.noreply.github.com>
2025-06-11 22:33:20 +0500

    first layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>App name</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <p>This is my body content.</p>
+    <div>
+        Hello
+    </div>
+@endsection
